6c51d10b864fbf32fa81b6725f6d9458dbc8c2b1
HeLao2006 <haole090706@gmail.com>
2025-11-03 21:31:01 +0700

    Lưu tạm workspace.xml và HighScoreManager trước khi pull

--- src/main/java/Ranking/HighScoreManager.java
+++ src/main/java/Ranking/HighScoreManager.java
@@ -11,15 +11,16 @@ import java.io.IOException;
 
 public class HighScoreManager {
     private static final int numScores = 10;
-    private static List<Integer> highScores = new ArrayList<>(numScores);
-
+    private static List<Integer> highScoresEasy = new ArrayList<>(numScores);
+    private static List<Integer> highScoresMedium = new ArrayList<>(numScores);
+    private static List<Integer> highScoresHard = new ArrayList<>(numScores);
 //    public HighScoreManager() {
-//        for (int i = 0; i < numScores; i++) highScores.add(0);
+//        for (int i = 0; i < numScores; i++) highScoresEasy.add(0);
 //    }
 
 
     public List<Integer> getHighScores() {
-        return highScores;
+        return highScoresEasy;
     }
 
     public void readFromFile() {
@@ -28,7 +29,7 @@ public class HighScoreManager {
             Scanner scanner = new Scanner(in);
             for (int i = 0; i < numScores; i++) {
                 int score = scanner.nextInt();
-                highScores.add(score);
+                highScoresEasy.add(score);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -38,24 +39,24 @@ public class HighScoreManager {
     public void addScore(int score) {
         int pos = -1;
 
-        if (score > highScores.get(0)) pos = 0;
-        for (int i = 0; i < highScores.size() - 1; i++) {
-            if (highScores.get(i) >= score && score > highScores.get(i + 1)) pos = i;
+        if (score > highScoresEasy.get(0)) pos = 0;
+        for (int i = 0; i < highScoresEasy.size() - 1; i++) {
+            if (highScoresEasy.get(i) >= score && score > highScoresEasy.get(i + 1)) pos = i;
         }
 
         if (pos == -1) return;
 
-        for (int i = highScores.size() - 1; i > pos; i--) {
-            highScores.set(i, highScores.get(i - 1));
+        for (int i = highScoresEasy.size() - 1; i > pos; i--) {
+            highScoresEasy.set(i, highScoresEasy.get(i - 1));
         }
 
-        highScores.set(pos, score);
+        highScoresEasy.set(pos, score);
     }
 
     public void writeToFile() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("HighScores.txt"))) {
             for (int i = 0; i < numScores; i++) {
-                bw.write(highScores.get(i) + "\n");
+                bw.write(highScoresEasy.get(i) + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
